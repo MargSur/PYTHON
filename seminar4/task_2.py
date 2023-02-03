8e67d5f9e278fec4962fb04c40e204242a708ba4
MargSur <suren1704@yandex.ru>
2023-02-03 18:55:48 +0300

    formatting

--- seminar4/task_2.py
+++ seminar4/task_2.py
@@ -3,10 +3,19 @@
 Функция должна принимать параметры как именованные аргументы.
 Реализовать вывод данных о пользователе одной строкой. """
 
+name = input('Ваше имя: ')
+surname = input('Ваша фамилия: ')
+year = int(input('Ваш год рождения: '))
+place = input('Ваш горож проживания: ')
+e_mail = input('Ваш e-mail: ')
+phone = input('Ваш номер телефона: ')
 
-def my_func(name, surname, year, city, email, telephone):
-    return " ".join([name, surname, year, city, email, telephone])
+
+def personal(arg_1, arg_2, arg_3, arg_4, arg_5, arg_6):
+    result = f'{name} {surname} {year} г.р., г.{place}, ' \
+             f'e-mail: {e_mail}, phone: {phone}'
+    return result
 
 
-print(my_func(name='Иван', surname='Иванов', year='2000', city='Москва',
-              email='i.i_@ya.ru', telephone='8-900-000-09-90'))
+print(personal(arg_1=name, arg_2=surname, arg_3=year,
+               arg_4=place, arg_5=e_mail, arg_6=phone))
