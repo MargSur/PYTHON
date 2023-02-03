46dfc50f0101c5e6f53b16685fca2ff7a57a3334
MargSur <suren1704@yandex.ru>
2023-02-03 19:46:11 +0300

    SOLVED

--- seminar4/task_3(2).py
+++ seminar4/task_3(2).py
@@ -1,20 +1,20 @@
 """ 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
 и возвращает сумму наибольших двух аргументов."""
 
-
 # Вариант 1. With SORT
+
 num1 = int(input('Введите первое число: '))
 num2 = int(input('Введите второе число: '))
 num3 = int(input('Введите третье число: '))
 
 
 def my_func(arg1, arg2, arg3):
-    if arg1 < arg2:
-        return arg2 + arg3
-    elif arg2 < arg1:
-        return arg1 + arg3
-    else:
-        return arg1 + arg2
+    lst = list()
+    lst.append(arg1)
+    lst.append(arg2)
+    lst.append(arg3)
+    lst.sort()
+    return lst[1]+lst[2]
 
 
 print(f'сумма наибольших двух чисел: {my_func(num1, num2, num3)}')
---
+++ seminar4/task_5.py
@@ -0,0 +1,22 @@
+""" 5. Сделайте профилирование кода любого или любых выполненных 
+заданий с помощью модуля timeit, опишите результат """
+
+from timeit import timeit
+
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+num3 = int(input('Введите третье число: '))
+
+
+def my_func(arg1, arg2, arg3):
+    lst = list()
+    lst.append(arg1)
+    lst.append(arg2)
+    lst.append(arg3)
+    lst.sort()
+    return lst[1]+lst[2]
+    
+
+
+print(f'сумма наибольших двух чисел: {my_func(num1, num2, num3)}')
+print(timeit('my_func', globals=globals(), number=100000))
