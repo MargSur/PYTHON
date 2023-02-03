43c671010f9304695f47590ebbf9cb49498ab517
MargSur <suren1704@yandex.ru>
2023-02-03 19:55:02 +0300

    fifth task

--- seminar4/task_5.py
+++ seminar4/task_5.py
@@ -15,8 +15,9 @@ def my_func(arg1, arg2, arg3):
     lst.append(arg3)
     lst.sort()
     return lst[1]+lst[2]
-    
 
 
 print(f'сумма наибольших двух чисел: {my_func(num1, num2, num3)}')
-print(timeit('my_func', globals=globals(), number=100000))
+print(timeit('my_func', globals=globals(), number=1000000))
+
+# Стоимость использования my_func=0.015979899966623634
