3c116a094e93e92f16e46de2bdf50b66a96c5369
MargSur <suren1704@yandex.ru>
2023-02-22 01:30:35 +0300

    8hw

--- seminar8/task_3.py
+++ seminar8/task_3.py
@@ -6,3 +6,17 @@ Four — 4
 Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл. """
+
+
+eng_origin = ["One", "Two", "Three", "Four"]
+rus_translate = ["Один", "Два", "Три", "Четыре"]
+
+with open('for_third_task.txt', 'r', encoding='utf-8') as file:
+    data = file.read()
+    print(data)
+    for i, word in enumerate(eng_origin):
+        if word in data:
+            data = data.replace(word, rus_translate[i])
+    print(data)
+with open('file_task_3end.txt', 'w', encoding='utf-8') as file:
+    file.write(data)
